7b194462e6b8a9d1067d0fc9c2751b184be3bbd1
Yerlys Daviel frias garcia <yerlysdavielfriasgarcia1@MacBook-Pro-de-Yerlys.local>
2018-04-18 17:15:21 +0200

    first version

---
+++ js.js
@@ -0,0 +1,12 @@
+function showhide(){
+    let div1 = document.getElementById('click1');
+    let div2 = document.getElementById('click2');
+    let div3 = document.getElementById('click3');
+    let div4 = document.getElementById('click4');
+    let div5 = document.getElementById('click5');
+    if(div1.style.display ==="none"){
+          div1.style.display = "block";
+    }else {
+        div1.style.display = "none";
+    }
+};
